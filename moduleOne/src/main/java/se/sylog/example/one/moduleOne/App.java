7b012701e02ca19d4d7294670ecb46630221aa88
Dennis Hjort <hjort.dennis@gmail.com>
2017-03-15 16:18:37 +0100

    Adding my first module into the sylog experiments repository

---
+++ moduleOne/src/main/java/se/sylog/example/one/moduleOne/App.java
@@ -0,0 +1,13 @@
+package se.sylog.example.one.moduleOne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
